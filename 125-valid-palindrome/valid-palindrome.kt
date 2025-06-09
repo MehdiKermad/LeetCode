11b5f0069777b8ec98dbf2fbd7219c168d156a7c
MehdiKermad <mkermad@live.fr>
2025-06-09 02:09:55 +0200

    Time: 16 ms (23.49%) | Memory: 46.4 MB (40.37%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.kt
+++ 125-valid-palindrome/valid-palindrome.kt
@@ -1,22 +1,19 @@
 class Solution {
     fun isPalindrome(s: String): Boolean {
         val stringBuilder = StringBuilder()
-        val stringBuilderReversed = StringBuilder()
 
         // We check if each character exist in Kotlin charsets
         for (c in s) {
             if (CharCategory.LOWERCASE_LETTER.contains(c) || c.isDigit()) {
                 stringBuilder.append(c)
-                stringBuilderReversed.insert(0, c)
                 continue
             }
             // If it's an uppercase we convert it
             if (CharCategory.UPPERCASE_LETTER.contains(c)) {
                 stringBuilder.append(c.lowercaseChar())
-                stringBuilderReversed.insert(0, c.lowercaseChar())
             }
         }
 
-        return stringBuilder.toString() == stringBuilderReversed.toString()
+        return stringBuilder.toString() == stringBuilder.toString().reversed()
     }
 }
