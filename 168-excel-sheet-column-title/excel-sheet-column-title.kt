b43bb07278847aef1a0c19f69e1906a7aadea0df
MehdiKermad <mkermad@live.fr>
2025-06-09 19:14:48 +0200

    Time: 3 ms (36.67%) | Memory: 41.4 MB (27.78%) - LeetSync

---
+++ 168-excel-sheet-column-title/excel-sheet-column-title.kt
@@ -0,0 +1,46 @@
+class Solution {
+    fun convertToTitle(columnNumber: Int): String {
+        val letterMap = mapOf(
+            1 to 'A',
+            2 to 'B',
+            3 to 'C',
+            4 to 'D',
+            5 to 'E',
+            6 to 'F',
+            7 to 'G',
+            8 to 'H',
+            9 to 'I',
+            10 to 'J',
+            11 to 'K',
+            12 to 'L',
+            13 to 'M',
+            14 to 'N',
+            15 to 'O',
+            16 to 'P',
+            17 to 'Q',
+            18 to 'R',
+            19 to 'S',
+            20 to 'T',
+            21 to 'U',
+            22 to 'V',
+            23 to 'W',
+            24 to 'X',
+            25 to 'Y',
+            26 to 'Z'
+        )
+        var title = ""
+        
+        var left = columnNumber
+        while (left > 0) {
+            // Compensate the absence of zero in our basis
+            left--
+            
+            val q = left/26
+            val r = left%26
+            
+            title = letterMap[1 + r].toString() + title
+            left = q
+        }
+        return title
+    }
+}
