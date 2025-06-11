b6c410263481fc3f2fb3db579a779fdcaf305f38
MehdiKermad <mkermad@live.fr>
2025-06-11 17:14:04 +0200

    Time: 8 ms (33.75%) | Memory: 41.8 MB (45.00%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.kt
@@ -0,0 +1,25 @@
+class Solution {
+    fun getRow(rowIndex: Int): List<Int> {
+        return generate(rowIndex + 1).last()
+    }
+
+    private fun generate(numRows: Int): List<List<Int>> {
+        val finalList = mutableListOf<List<Int>>()
+        finalList.add(listOf(1))
+        
+        for (i in 1 until numRows) {
+            // A list size corresponds to n + 1
+            val listSize = i + 1
+            val currentList = MutableList<Int>(listSize) { 1 }
+
+            // Aside of the extremes, each cell is calculated with n^i-1 + n^i cells of the previous one
+            for (j in 1 until listSize - 1) {
+                currentList[j] = finalList[i - 1][j - 1] + finalList[i - 1][j]
+            }
+
+            finalList.add(currentList)
+        }
+        
+        return finalList
+    }
+}
