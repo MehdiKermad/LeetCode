95cccc5bf37980724fdb028393a1970220093b44
MehdiKermad <mkermad@live.fr>
2025-06-13 17:33:04 +0200

    Time: 15 ms (62.46%) | Memory: 44.8 MB (21.41%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.kt
+++ 205-isomorphic-strings/isomorphic-strings.kt
@@ -1,29 +1,21 @@
 class Solution {
     fun isIsomorphic(s: String, t: String): Boolean {
-        val lettersMap = mutableMapOf<Char, Char>()
-        val lettersMap2 = mutableMapOf<Char, Char>()
+        val firstMap = mutableMapOf<Char, Char>()
+        val secondMap = mutableMapOf<Char, Char>()
         
         // Both words have the same size
         for (i in 0 until s.length) {
             
             // We check the first matching
-            val firstMatch = lettersMap[s[i]]
-            if (firstMatch == null) {
-                lettersMap[s[i]] = t[i]
-            } else {
-                if (firstMatch != t[i]) {
-                    return false
-                }
+            val firstMatch = firstMap.getOrPut(s[i]) { t[i] }
+            if (firstMatch != t[i]) {
+                return false
             }
             
             // We check the second matching
-            val secondMatch = lettersMap2[t[i]]
-            if (secondMatch == null) {
-				lettersMap2[t[i]] = s[i]
-            } else {
-                if (secondMatch != s[i]) {
-					return false
-                }
+            val secondMatch = secondMap.getOrPut(t[i]) { s[i] }
+            if (secondMatch != s[i]) {
+                return false
             }
         }
 
