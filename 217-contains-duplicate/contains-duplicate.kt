c5d005f6b5048ed5ad89b984c1da48dedb3fe2af
MehdiKermad <mkermad@live.fr>
2025-06-13 18:08:37 +0200

    Time: 25 ms (50.90%) | Memory: 66.1 MB (42.07%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun containsDuplicate(nums: IntArray): Boolean {
+        val set = mutableSetOf<Int>()
+        for (i in nums) {
+            if (!set.contains(i)) {
+                set.add(i)
+            } else {
+                return true
+            }
+        }
+        return false  
+    }
+}
