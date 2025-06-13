34ec24e51a61c0d67f053ac096bbb473bced999a
MehdiKermad <mkermad@live.fr>
2025-06-13 18:50:26 +0200

    Time: 41 ms (32.04%) | Memory: 67.1 MB (13.84%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.kt
+++ 219-contains-duplicate-ii/contains-duplicate-ii.kt
@@ -4,16 +4,15 @@ class Solution {
         
         // Checking each number position
         for (i in 0 until nums.size) {
-            if (numberPosMap.contains(nums[i])) {
-                if (Math.abs(numberPosMap[nums[i]]!! - i) <= k) {
+            val numberPositionMatch = numberPosMap.getOrPut(nums[i]) { i }
+            if (numberPositionMatch != i) {
+                if (Math.abs(numberPositionMatch - i) <= k) {
                     // Checking if the previous occurence is in the range
                     return true
                 } else {
                     // Or we update the position with the new one
                     numberPosMap[nums[i]] = i
                 }
-            } else {
-                numberPosMap[nums[i]] = i
             }
         }
         
