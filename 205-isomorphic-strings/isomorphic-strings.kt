906aed1fce1b365f11a2fb1f49e35ec2518ea5b8
MehdiKermad <mkermad@live.fr>
2025-06-13 17:20:06 +0200

    Time: 30 ms (11.14%) | Memory: 47.9 MB (5.28%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.kt
@@ -0,0 +1,29 @@
+class Solution {
+    fun isIsomorphic(s: String, t: String): Boolean {
+        val lettersMap = mutableMapOf<Char, Char>()
+        val lettersMap2 = mutableMapOf<Char, Char>()
+        for (i in 0 until s.length) {
+            if (!lettersMap.contains(s[i])) {
+                println(""+s[i]+ " => " + t[i])
+                lettersMap[s[i]] = t[i]
+            } else {
+                if (lettersMap[s[i]] != t[i]) {
+                    println(""+lettersMap[s[i]] + " est diff de " + t[i])
+                    return false
+                }
+            }
+            
+            if (!lettersMap2.contains(t[i])) {
+                println(""+t[i]+ " => " + s[i])
+				lettersMap2[t[i]] = s[i]
+            } else {
+                if (lettersMap2[t[i]] != s[i]) {
+                    println(""+lettersMap2[t[i]] + " est diff de " + s[i])
+					return false
+                }
+            }
+        }
+
+        return true
+    }
+}
