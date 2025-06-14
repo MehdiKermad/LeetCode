79965fddbb1f4a3e6712683288215cb2bbd2e829
MehdiKermad <mkermad@live.fr>
2025-06-14 18:04:52 +0200

    Time: 7 ms (30.94%) | Memory: 42.7 MB (29.32%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.kt
@@ -0,0 +1,43 @@
+class Solution {
+    fun summaryRanges(nums: IntArray): List<String> {
+
+        // If it's empty, we return an empty list
+        if (nums.isEmpty()) return listOf()
+
+        val rangesList = mutableListOf<String>()
+        var firstElement = nums[0]
+        var lastElement: Int? = null
+
+        for (i in 1 until nums.size) {
+            val diff = Math.abs(nums[i] - nums[i-1])
+            println("Diff entre "+nums[i-1]+ " et "+ nums[i] + " = "+ diff)
+            if (diff == 1) {
+                // If the difference between n - n-1 = 1, it's the same range
+                println("-- Range++")
+                lastElement = nums[i]
+            } else if (diff != 1 && lastElement == null) {
+                // Case where we start with an isolated number
+                println("-- Isolate")
+                rangesList.add(firstElement.toString())
+                firstElement = nums[i]
+            } else {
+                // Case where the range breaks
+                println("-- Breaks")
+                rangesList.add("" + firstElement + "->" + nums[i-1])
+                firstElement = nums[i]
+                lastElement = null
+            }
+        }
+
+        // Checking on the last element
+        if (lastElement != null) {
+            // Ranges case
+            rangesList.add("" + firstElement + "->" + lastElement)
+        } else {
+            // Isolated number
+            rangesList.add(firstElement.toString())
+        }
+
+        return rangesList
+    }
+}
