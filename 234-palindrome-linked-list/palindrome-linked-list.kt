2a15afeb3a2f0385675de3439fb6c8c9c62457ea
MehdiKermad <mkermad@live.fr>
2025-06-14 18:53:53 +0200

    Time: 24 ms (14.06%) | Memory: 67.5 MB (12.89%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.kt
+++ 234-palindrome-linked-list/palindrome-linked-list.kt
@@ -13,23 +13,14 @@ class Solution {
         // If there is one or no element, it's a palindrome
         if (head == null || head?.next == null) return true
 
-        val deque = mutableListOf<Int>()
-        //val reversedDeque = mutableListOf<Int>()
+        val list = mutableListOf<Int>()
         var currentNode = head
 
         while (currentNode != null) {
-            /* if (currentNode?.`val` == deque.lastOrNull()) {
-                println("On retire " + currentNode?.`val` + " de " + deque.toString())
-                deque.removeLast()
-            } else {
-                println("On ajoute " + currentNode?.`val` + " a " + deque.toString())
-                deque.add(currentNode?.`val`!!)
-            }*/
-            deque.add(currentNode?.`val`!!)
-            //reversedDeque.add(0, currentNode?.`val`!!)
+            list.add(currentNode?.`val`!!)
             currentNode = currentNode?.next
         }
 
-        return deque == deque.reversed()
+        return list == list.reversed()
     }
 }
