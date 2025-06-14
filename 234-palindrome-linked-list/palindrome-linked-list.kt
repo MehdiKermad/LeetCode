c81d443f6b1dd3cc19bf114e1cc67d86556cbe04
MehdiKermad <mkermad@live.fr>
2025-06-14 18:46:36 +0200

    Time: 50 ms (5.08%) | Memory: 64.1 MB (22.27%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.kt
@@ -0,0 +1,33 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun isPalindrome(head: ListNode?): Boolean {
+
+        // If there is one or no element, it's a palindrome
+        if (head == null || head?.next == null) return true
+
+        val deque = ArrayDeque<Int>()
+        var currentNode = head
+
+        while (currentNode != null) {
+            /* if (currentNode?.`val` == deque.lastOrNull()) {
+                println("On retire " + currentNode?.`val` + " de " + deque.toString())
+                deque.removeLast()
+            } else {
+                println("On ajoute " + currentNode?.`val` + " a " + deque.toString())
+                deque.add(currentNode?.`val`!!)
+            }*/
+            deque.add(currentNode?.`val`!!)
+            currentNode = currentNode?.next
+        }
+
+        return deque == deque.reversed()
+    }
+}
