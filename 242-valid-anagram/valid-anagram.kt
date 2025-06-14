6562a0259be7685a236e49c630305227b08c50e1
MehdiKermad <mkermad@live.fr>
2025-06-14 19:44:00 +0200

    Time: 26 ms (36.61%) | Memory: 47.4 MB (17.40%) - LeetSync

--- 242-valid-anagram/valid-anagram.kt
+++ 242-valid-anagram/valid-anagram.kt
@@ -1,5 +1,18 @@
 class Solution {
     fun isAnagram(s: String, t: String): Boolean {
-        return s.length == t.length && s.groupingBy { it }.eachCount() == t.groupingBy { it }.eachCount()
+
+        if (s.length != t.length) return false
+
+        val firstFrequency = mutableMapOf<Char, Int>()
+        val secondFrequency = mutableMapOf<Char, Int>()
+        
+        for (i in 0 until s.length) {
+            val firstCount = firstFrequency.getOrPut(s[i]) { 0 }
+            firstFrequency[s[i]] = firstCount+1
+            val secondCount = secondFrequency.getOrPut(t[i]) { 0 }
+            secondFrequency[t[i]] = secondCount+1
+        }
+        
+        return firstFrequency == secondFrequency
     }
 }
