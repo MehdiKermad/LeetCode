8695dd507349689912dbd991d36596fb919b2251
MehdiKermad <mkermad@live.fr>
2025-06-18 15:43:14 +0200

    Time: 3 ms (45.01%) | Memory: 49.2 MB (97.15%) - LeetSync

--- 283-move-zeroes/move-zeroes.kt
+++ 283-move-zeroes/move-zeroes.kt
@@ -2,11 +2,11 @@ class Solution {
     fun moveZeroes(nums: IntArray): Unit {
         var k = 0
         for (i in 0 until nums.size) {
-            if (nums[i] != 0) {
+            val currentVal = nums[i]
+            if (currentVal != 0) {
                 val oldValue = nums[k]
-                nums[k] = nums[i]
+                nums[k++] = currentVal
                 nums[i] = oldValue
-                k++
             }
         }
     }
