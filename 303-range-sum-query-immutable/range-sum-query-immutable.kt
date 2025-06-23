43ad9fed7a9e956a84baddf0a4cc6a3790bb0e81
MehdiKermad <mkermad@live.fr>
2025-06-23 10:15:45 +0200

    Time: 70 ms (14.46%) | Memory: 55.5 MB (13.25%) - LeetSync

---
+++ 303-range-sum-query-immutable/range-sum-query-immutable.kt
@@ -0,0 +1,21 @@
+class NumArray(nums: IntArray) {
+    val nums: IntArray
+    init {
+        this.nums = nums
+    }
+
+    fun sumRange(left: Int, right: Int): Int {
+        var sum: Int = 0
+        for (n in left..right) {
+            sum += nums[n]
+        }
+        return sum
+    }
+
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
