bebbebe685c21d50d493b99331f5553b1056e9e1
MehdiKermad <mkermad@live.fr>
2025-06-25 15:57:33 +0200

    Time: 18 ms (10.44%) | Memory: 51 MB (80.77%) - LeetSync

--- 338-counting-bits/counting-bits.kt
+++ 338-counting-bits/counting-bits.kt
@@ -2,10 +2,21 @@ class Solution {
     fun countBits(n: Int): IntArray {
         val bitArray = Array<Int>(n + 1) { 0 } // Fixed size is more performant
         
+        // We fill the array from 0 to n + 1
         for (i in 0 .. n) {
-            bitArray[i] = i.toString(2).count { it == '1'}
+            bitArray[i] = toBinary(i) // The number to calculate is the current indice
         }
         
         return bitArray.toIntArray()
+    }
+
+    private fun toBinary(n: Int): Int {
+        var res = n
+        var posCount = 0 // Number of of bit worth 1
+        while (res > 0) {
+            posCount += res%2
+            res /= 2
+        }
+        return posCount
     }
 }
