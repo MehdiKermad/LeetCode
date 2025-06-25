b79396f550fd35397a7847a276b40afa5868d5d1
MehdiKermad <mkermad@live.fr>
2025-06-25 19:40:08 +0200

    Time: 1 ms (83.02%) | Memory: 41.1 MB (54.72%) - LeetSync

--- 263-ugly-number/ugly-number.kt
+++ 263-ugly-number/ugly-number.kt
@@ -1,28 +1,32 @@
 class Solution {
     fun isUgly(n: Int): Boolean {
 
-        // An ugly number has to be positive
         if (n < 1) { 
+            // An ugly number has to be positive
             return false
         } else if (n == 1) {
-            return true}
+            // 1 has only itself as a prime factor
+            return true
+        }
 
         var left = n
+
+        // We keep dividing the number by 2, 3 or 5 as long as it's possible
         while (left != 1) {
             when {
                 left % 2 == 0 -> {
-                    println("$left est un multiple de 2")
                     left /= 2
                 }
                 left % 3 == 0 -> {
-                    println("$left est un multiple de 3")
                     left /= 3
                 }
                 left % 5 == 0 -> {
-                    println("$left est un multiple de 5")
                     left /= 5
                 }
-                else -> return false
+                else -> {
+                    // If the number is not divisible by 2/3/5, it means we found a prime factor
+                    return false
+                }
             }
         }
         
