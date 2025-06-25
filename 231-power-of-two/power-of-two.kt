437f50a31732db69c3475773c3fd8252deb40c1c
MehdiKermad <mkermad@live.fr>
2025-06-25 16:03:35 +0200

    Time: 1 ms (63.87%) | Memory: 40.5 MB (100.00%) - LeetSync

--- 231-power-of-two/power-of-two.kt
+++ 231-power-of-two/power-of-two.kt
@@ -1,12 +1,11 @@
 class Solution {
     fun isPowerOfTwo(n: Int): Boolean {        
-        var factor: Long = 1 // We need to use long type for a bigger value range
-        while (factor < n) {
-            // We multiply the number until we reach n or go over it
-            factor *= 2
+        if (n == 0) return false
+
+        var res: Long = 1
+        while (res < n) {
+            res *= 2
         }
-        
-        // If the last operation is equals to n, it's a power of two
-        return factor == n.toLong()
+        return res == n.toLong()
     }
 }
