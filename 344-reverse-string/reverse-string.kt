7e2b79c92dc2456d9a257479e62976d4fb0adff7
MehdiKermad <mkermad@live.fr>
2025-06-25 17:19:27 +0200

    Time: 1 ms (92.05%) | Memory: 52.8 MB (75.38%) - LeetSync

--- 344-reverse-string/reverse-string.kt
+++ 344-reverse-string/reverse-string.kt
@@ -1,9 +1,10 @@
 class Solution {
     fun reverseString(s: CharArray): Unit {
-        for (i in 0 until s.size / 2) {
+        val stringSize = s.size
+        for (i in 0 until stringSize / 2) {
             val tmp = s[i]
-            s[i] = s[s.size - i - 1]
-            s[s.size - i - 1] = tmp
+            s[i] = s[stringSize - i - 1]
+            s[stringSize - i - 1] = tmp
         }
     }
 }
