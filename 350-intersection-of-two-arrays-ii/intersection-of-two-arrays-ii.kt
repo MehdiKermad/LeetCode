4100910c6141003fc8814122042aa5b31a3b6694
MehdiKermad <mkermad@live.fr>
2025-06-28 01:09:57 +0200

    Time: 19 ms (13.92%) | Memory: 46.9 MB (7.69%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.kt
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.kt
@@ -1,39 +1,28 @@
 class Solution {
     fun intersect(nums1: IntArray, nums2: IntArray): IntArray {
+        return if (nums1.size < nums2.size) {
+            intersectArrays(nums1, nums2)
+        } else {
+            intersectArrays(nums2, nums1)
+        }
+    }
+
+    private fun intersectArrays(shortArr: IntArray, longArr: IntArray): IntArray {
         val seenList = mutableListOf<Int>()
         val interList = mutableListOf<Int>()
+        
+        // We build a list of seen values with the shortest array
+        seenList.addAll(shortArr.toTypedArray())
 
-        if (nums1.size < nums2.size) {
-            // We build a list of seen values
-            nums1.forEach {
-                seenList.add(it)
-            }
-
-            // Then we look at each value to determine which one is common
-            for (i in nums2) {
-                if (seenList.contains(i)) {
-                    seenList.remove(i)
-                    interList.add(i)
-                    if (seenList.isEmpty()) break
-                }
-            }
-        } else {
-            // We build a list of seen values
-            nums2.forEach {
-                seenList.add(it)
-            }
-
-            // Then we look at each value to determine which one is common
-            for (i in nums1) {
-                if (seenList.contains(i)) {
-                    seenList.remove(i)
-                    interList.add(i)
-                    if (seenList.isEmpty()) break
-                }
+        // Then we look at each value to determine which one is common
+        for (i in longArr) {
+            if (seenList.contains(i)) {
+                interList.add(i)
+                seenList.remove(i)
+                if (seenList.isEmpty()) break
             }
         }
-    
-
+        
         return interList.toIntArray()
     }
 }
