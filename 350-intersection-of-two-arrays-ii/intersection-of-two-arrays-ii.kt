37c117e46e6b53bcbbcec4002ff91dc1c79f7602
MehdiKermad <mkermad@live.fr>
2025-06-28 00:58:57 +0200

    Time: 14 ms (32.60%) | Memory: 45.4 MB (61.17%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.kt
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.kt
@@ -3,18 +3,38 @@ class Solution {
         val seenList = mutableListOf<Int>()
         val interList = mutableListOf<Int>()
 
-        // We build a list of seen values
-        nums1.forEach {
-            seenList.add(it)
-        }
+        if (nums1.size < nums2.size) {
+            // We build a list of seen values
+            nums1.forEach {
+                seenList.add(it)
+            }
 
-        // Then we look at each value to determine which one is common
-        nums2.forEach {
-            if (seenList.contains(it)) {
-                seenList.remove(it)
-                interList.add(it)
+            // Then we look at each value to determine which one is common
+            var cpt = 0
+            while (seenList.isNotEmpty() && cpt < nums2.size) {
+                if (seenList.contains(nums2[cpt])) {
+                    seenList.remove(nums2[cpt])
+                    interList.add(nums2[cpt])
+                }
+                cpt++
+            }
+        } else {
+            // We build a list of seen values
+            nums2.forEach {
+                seenList.add(it)
+            }
+
+            // Then we look at each value to determine which one is common
+            var cpt = 0
+            while (seenList.isNotEmpty() && cpt < nums1.size) {
+                if (seenList.contains(nums1[cpt])) {
+                    seenList.remove(nums1[cpt])
+                    interList.add(nums1[cpt])
+                }
+                cpt++
             }
         }
+    
 
         return interList.toIntArray()
     }
