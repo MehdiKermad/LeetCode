a556798539eaf7bbf18341ed4bd873db89538336
MehdiKermad <mkermad@live.fr>
2025-06-28 00:26:06 +0200

    Time: 21 ms (42.76%) | Memory: 47.3 MB (11.03%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.kt
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.kt
@@ -1,8 +1,32 @@
 class Solution {
     fun intersection(nums1: IntArray, nums2: IntArray): IntArray {
-        val firstSet = nums1.toSet()
-        val secondSet = nums2.toSet()
-
-        return firstSet.filter { secondSet.contains(it) }.toIntArray()
+        val maxSize = maxOf(nums1.size, nums2.size)
+        val firstNumsSet = mutableSetOf<Int>()
+        val secondNumsSet = mutableSetOf<Int>()
+        val interSet = mutableSetOf<Int>()
+        
+        // We check both arrays simultanously 
+        for (i in 0 until maxSize) {
+            val currentFirst = nums1.getOrNull(i)
+            val currentSecond = nums2.getOrNull(i)
+            
+            // We add everything we find in sets
+            currentFirst?.let {
+                firstNumsSet.add(it)
+                
+                // If it's present in the other set, it's a common element
+                if (secondNumsSet.contains(it)) {
+                    interSet.add(it)
+                }
+            }
+            currentSecond?.let {
+                secondNumsSet.add(it)
+                if (firstNumsSet.contains(it)) {
+                    interSet.add(it)
+                }
+            }
+        }
+        
+        return interSet.toIntArray()
     }
 }
