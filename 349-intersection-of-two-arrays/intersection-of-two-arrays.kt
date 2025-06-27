35752a536cde05da796806b53c3ea91ca496f404
MehdiKermad <mkermad@live.fr>
2025-06-28 00:29:19 +0200

    Time: 24 ms (15.52%) | Memory: 47.3 MB (13.45%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.kt
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.kt
@@ -1,8 +1,8 @@
 class Solution {
     fun intersection(nums1: IntArray, nums2: IntArray): IntArray {
         val maxSize = maxOf(nums1.size, nums2.size)
-        val firstNumsSet = mutableSetOf<Int>()
-        val secondNumsSet = mutableSetOf<Int>()
+        val firstNumsSet = mutableSetOf<Int?>()
+        val secondNumsSet = mutableSetOf<Int?>()
         val interSet = mutableSetOf<Int>()
         
         // We check both arrays simultanously 
@@ -11,19 +11,16 @@ class Solution {
             val currentSecond = nums2.getOrNull(i)
             
             // We add everything we find in sets
-            currentFirst?.let {
-                firstNumsSet.add(it)
+            firstNumsSet.add(currentFirst)
                 
-                // If it's present in the other set, it's a common element
-                if (secondNumsSet.contains(it)) {
-                    interSet.add(it)
-                }
+            // If it's present in the other set, it's a common element
+            if (secondNumsSet.contains(currentFirst)) {
+                interSet.add(currentFirst!!)
             }
-            currentSecond?.let {
-                secondNumsSet.add(it)
-                if (firstNumsSet.contains(it)) {
-                    interSet.add(it)
-                }
+            
+            secondNumsSet.add(currentSecond)
+            if (firstNumsSet.contains(currentSecond)) {
+                interSet.add(currentSecond!!)
             }
         }
         
