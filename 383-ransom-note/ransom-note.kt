6bf9b5187479e071d970d6a44d80fc8aa7281bc1
MehdiKermad <mkermad@live.fr>
2025-07-01 00:49:48 +0200

    Time: 29 ms (15.16%) | Memory: 47.4 MB (19.60%) - LeetSync

--- 383-ransom-note/ransom-note.kt
+++ 383-ransom-note/ransom-note.kt
@@ -4,25 +4,14 @@ class Solution {
         if (ransomNote.isEmpty()) return true
         if (magazine.length < ransomNote.length) return false
         
-        // We count letters needed and available
-        val neededLetters = mutableMapOf<Char, Int>()
-        val availableLetters = mutableMapOf<Char, Int>()
+        // We transform needed letters as a list
+        val neededLetters = ransomNote.toList()
+        val availableLetters = magazine.toMutableList()
         
-        // We count letters needed and available
-        for (i in 0 until magazine.length) {
-            val neededChar = ransomNote.getOrNull(i)
-            val availableChar = magazine[i]
-            
-            neededChar?.let {
-                neededLetters[it] = neededLetters.getOrDefault(it, 0) + 1
-            }
-            availableLetters[availableChar] = availableLetters.getOrDefault(availableChar, 0) + 1
-        }
-        
-        // For each letter needed, we check if we have enough of them
-        for (entry in neededLetters) {
-            val availableLetterCount = availableLetters.getOrDefault(entry.key, 0)
-            if (availableLetterCount < entry.value) {
+        // We try to remove each letter needed of available letters
+        for (c in neededLetters) {
+            // If we can't, it means this letter is missing
+            if (availableLetters.remove(c) == false) {
                 return false
             }
         }
