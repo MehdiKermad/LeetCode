13e3d9967598ef809f101efbc99325e65995f391
MehdiKermad <mkermad@live.fr>
2025-07-01 00:44:21 +0200

    Time: 33 ms (10.87%) | Memory: 48.2 MB (5.27%) - LeetSync

--- 383-ransom-note/ransom-note.kt
+++ 383-ransom-note/ransom-note.kt
@@ -5,8 +5,19 @@ class Solution {
         if (magazine.length < ransomNote.length) return false
         
         // We count letters needed and available
-        val neededLetters = ransomNote.groupingBy { it }.eachCount()
-        val availableLetters = magazine.groupingBy { it }.eachCount()
+        val neededLetters = mutableMapOf<Char, Int>()
+        val availableLetters = mutableMapOf<Char, Int>()
+        
+        // We count letters needed and available
+        for (i in 0 until magazine.length) {
+            val neededChar = ransomNote.getOrNull(i)
+            val availableChar = magazine[i]
+            
+            neededChar?.let {
+                neededLetters[it] = neededLetters.getOrDefault(it, 0) + 1
+            }
+            availableLetters[availableChar] = availableLetters.getOrDefault(availableChar, 0) + 1
+        }
         
         // For each letter needed, we check if we have enough of them
         for (entry in neededLetters) {
