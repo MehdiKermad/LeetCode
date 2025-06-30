753fd72496620291307939fe260cb082ea29e003
MehdiKermad <mkermad@live.fr>
2025-07-01 00:59:47 +0200

    Time: 17 ms (56.84%) | Memory: 46 MB (74.79%) - LeetSync

--- 383-ransom-note/ransom-note.kt
+++ 383-ransom-note/ransom-note.kt
@@ -1,12 +1,15 @@
 class Solution {
     fun canConstruct(ransomNote: String, magazine: String): Boolean {
         // We transform needed letters as a list
-        val availableLetters = magazine.toMutableList()
+        var availableLetters = StringBuilder(magazine)
         
         // We try to remove each letter needed of available letters
         for (c in ransomNote) {
+            val charPos = availableLetters.indexOf(c)
             // If we can't, it means this letter is missing
-            if (availableLetters.remove(c) == false) {
+            if (charPos > -1) {
+                availableLetters = availableLetters.deleteAt(charPos)
+            } else {
                 return false
             }
         }
