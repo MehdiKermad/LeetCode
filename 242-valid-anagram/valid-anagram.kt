3f50f6f1b5323a395c53cff42707dd33500e28ea
MehdiKermad <mkermad@live.fr>
2025-07-05 16:34:56 +0200

    Time: 6 ms (87.08%) | Memory: 43.6 MB (86.27%) - LeetSync

--- 242-valid-anagram/valid-anagram.kt
+++ 242-valid-anagram/valid-anagram.kt
@@ -1,18 +1,21 @@
 class Solution {
     fun isAnagram(s: String, t: String): Boolean {
-
+        // If strings don't have the same size they can't be anagram
         if (s.length != t.length) return false
 
-        val firstFrequency = mutableMapOf<Char, Int>()
-        val secondFrequency = mutableMapOf<Char, Int>()
-        
+        val aVal = 'a'.toInt()
+        var sArray = IntArray(size = 26) { 0 }
+        var tArray = IntArray(size = 26) { 0 }
+
+        // We get the numeric value of each char
         for (i in 0 until s.length) {
-            val firstCount = firstFrequency.getOrPut(s[i]) { 0 }
-            firstFrequency[s[i]] = firstCount+1
-            val secondCount = secondFrequency.getOrPut(t[i]) { 0 }
-            secondFrequency[t[i]] = secondCount+1
+            val sVal = s[i].toInt()
+            val tVal = t[i].toInt()
+            
+            sArray[sVal - aVal] = sArray[sVal - aVal] + 1
+            tArray[tVal - aVal] = tArray[tVal - aVal] + 1
         }
         
-        return firstFrequency == secondFrequency
+        return sArray.contentEquals(tArray)
     }
 }
