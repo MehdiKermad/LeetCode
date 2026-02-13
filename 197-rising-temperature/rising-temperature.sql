a4a7a1b2debbd80f0edea70a4d589c907a130319
MehdiKermad <mkermad@live.fr>
2026-02-14 00:58:21 +0100

    Time: 990 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,8 +1,16 @@
 # Write your MySQL query statement below
+/*SELECT n.id
+FROM Weather AS n
+WHERE n.temperature > (
+    SELECT t.temperature
+    FROM Weather AS t
+    WHERE t.id = (n.id - 1)
+)*/
+
 SELECT n.id
 FROM Weather AS n
 WHERE n.temperature > (
     SELECT t.temperature
     FROM Weather AS t
-    WHERE t.recordDate = DATE_SUB(n.recordDate, INTERVAL 1 DAY)
+    WHERE (DATEDIFF(n.recordDate, t.recordDate)) = 1
 )
