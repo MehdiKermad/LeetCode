4b99ec28e531ef82131016a667d22556b0d43c62
MehdiKermad <mkermad@live.fr>
2026-02-14 01:03:15 +0100

    Time: 511 ms (24.92%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,16 +1,6 @@
 # Write your MySQL query statement below
-/*SELECT n.id
-FROM Weather AS n
-WHERE n.temperature > (
-    SELECT t.temperature
-    FROM Weather AS t
-    WHERE t.id = (n.id - 1)
-)*/
-
-SELECT n.id
-FROM Weather AS n
-WHERE n.temperature > (
-    SELECT t.temperature
-    FROM Weather AS t
-    WHERE (DATEDIFF(n.recordDate, t.recordDate)) = 1
-)
+SELECT today.id
+FROM Weather AS yesterday 
+CROSS JOIN Weather AS today
+WHERE DATEDIFF(today.recordDate, yesterday.recordDate) = 1
+AND today.temperature > yesterday.temperature;
